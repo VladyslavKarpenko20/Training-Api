5b22eb2856096a8da706dd28af152a41923eb6b9
VladKarpenko <vladkarpenko0707@gmail.com>
2026-06-18 22:32:00 +0300

    Add database connection and User models

---
+++ Training Api/Context/AddDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Training_Api.Models;
+
+
+namespace Training_Api.Context
+{
+    public class AddDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+
+        public AddDbContext(DbContextOptions<AddDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Training Api/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Training_Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
